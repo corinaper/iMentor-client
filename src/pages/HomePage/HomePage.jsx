03a792fc253aa80adf427d240530fe9797529e80
Klaus Haugnaess <klaushaugness@gmail.com>
2022-06-01 09:47:10 +0200

    homepage updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,29 @@
-import { Container, Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const IndexPage = () => {
 
     return (
-        <Container>
-            <h1>¡Bienvenid@ a la página de las coasters!</h1>
-            <hr />
-            <Link to="/galeria">
-                <Button variant="dark" size='lg'>Ir a la galería</Button>
-            </Link>
-        </Container>
+    <div>
+        <h1>iMentor</h1>
+        <br/>
+        <br/>
+        <h5>Do you need a mentor?
+        <br/>
+        <br/>
+        Thats why we here!
+        <br/>
+        <br/>
+        Sign up or login here</h5>
+        <Link to="/login">
+            <Button variant="dark" size='lg'>Login</Button>
+        </Link>
+        <br/>
+        <br/>
+        <Link to="/signup">
+            <Button variant="dark" size='lg'>Sign Up</Button>
+        </Link>
+    </div>
     )
 }
 
