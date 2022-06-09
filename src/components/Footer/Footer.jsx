bde590ab57a96636c08e5f22e91de18248729bfc
Alberte Vieites <albertevieites@tutanota.com>
2022-06-09 12:57:42 +0200

    css wip 1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,37 @@
-import './Footer.css'
 import { Link } from 'react-router-dom'
 import { useContext } from 'react'
 import { AuthContext } from '../../context/auth.context'
+import "./Footer.css";
 
 const Footer = () => {
 
 const { user } = useContext(AuthContext)
 
 if (user)
-return(
-     
-    <footer className='footer'>
-        
-        <Link to="/questions" className="nav-link">Questions</Link>
-         <Link to="/mentors" className="nav-link">Mentor</Link>
-          <Link to={`/profile/${user._id}`}  className="nav-link">Profile</Link>
-         
-    </footer>
+return (
+    <footer className="footer">
+        <nav>
+            <ul className="nav-links">
+                <li>
+                    <Link to="/questions" className="nav-link">
+                        Questions
+                    </Link>
+                </li>
 
-)};
+                <li>
+                    <Link to="/mentors" className="nav-link">
+                        Mentor
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to={`/profile/${user._id}`} className="nav-link">
+                            Profile
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+);};
 
 export default Footer
